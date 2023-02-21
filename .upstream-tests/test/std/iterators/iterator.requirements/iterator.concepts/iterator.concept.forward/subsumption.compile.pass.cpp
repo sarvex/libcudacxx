bf48b124385db78e2ae02c1b9b943dd38e81a720
Michael Schellenberger Costa <miscco@nvidia.com>
2023-02-21 14:47:38 +0100

    Implement iterator concepts

--- .upstream-tests/test/std/iterators/iterator.requirements/iterator.concepts/iterator.concept.forward/subsumption.compile.pass.cpp
+++ .upstream-tests/test/std/iterators/iterator.requirements/iterator.concepts/iterator.concept.forward/subsumption.compile.pass.cpp
@@ -7,8 +7,31 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// XFAIL: c++20
+// nvbug 3885350
+
+// cuda::std::forward_iterator;
+
+#include <cuda/std/iterator>
+
+#include <cuda/std/concepts>
+
+// clang-format off
+template<cuda::std::input_iterator>
+__host__ __device__ constexpr bool check_subsumption() {
+  return false;
+}
+
+template<cuda::std::forward_iterator>
+__host__ __device__ constexpr bool check_subsumption() {
+  return true;
+}
+// clang-format on
+
+static_assert(check_subsumption<int*>());
+
 int main(int, char**)
 {
-
   return 0;
 }
